0aeb143143879b30c82eaeb15c7fdb1fd9f3bcc4
Maja Pawlina <maja.anna.pawlina@gmail.com>
2022-12-03 21:30:32 +0100

    Added GameSimulator and WinningSchemeProcessor

---
+++ src/main/java/com/github/maypaw/advent_of_code/day2/GameSimulator.java
@@ -0,0 +1,11 @@
+package com.github.maypaw.advent_of_code.day2;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class GameSimulator {
+
+    private final Long playerScore = 0L;
+    private final Long opponentScore = 0L;
+
+}
---
+++ src/main/java/com/github/maypaw/advent_of_code/day2/WinningSchemeProcessor.java
@@ -0,0 +1,9 @@
+package com.github.maypaw.advent_of_code.day2;
+
+public class WinningSchemeProcessor {
+
+    private WinningSchemeProcessor() {
+
+    }
+
+}
