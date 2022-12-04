089de27fe45ead79026521508bc666dbdbca82a3
Maja Pawlina <maja.anna.pawlina@gmail.com>
2022-12-04 01:01:33 +0100

    Added tests

---
+++ src/test/java/com/github/maypaw/advent_of_code/day2/GameSimulatorTest.java
@@ -0,0 +1,89 @@
+package com.github.maypaw.advent_of_code.day2;
+
+import org.junit.jupiter.api.Test;
+
+import static com.github.maypaw.advent_of_code.day2.GameState.DRAW;
+import static com.github.maypaw.advent_of_code.day2.GameState.VICTORY;
+import static com.github.maypaw.advent_of_code.day2.Move.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class GameSimulatorTest {
+
+    @Test
+    void shouldReturnValidSum_WhenCountingPlayerPoints() {
+        // given
+        Move[][] scheme = {{A, Y}, {B, X}, {C, Z}};
+        Long expectedScore = 15L;
+
+        // when
+        Long playerTotalScore = GameSimulator.getPlayerTotalScore(scheme);
+
+        // then
+        assertThat(playerTotalScore).isEqualTo(expectedScore);
+
+    }
+
+    @Test
+    void shouldReturnZero_WhenGivenEmptyScheme() {
+        // given
+        Move[][] scheme = {};
+        // when
+        Long playerTotalScore = GameSimulator.getPlayerTotalScore(scheme);
+
+        // then
+        assertThat(playerTotalScore).isZero();
+    }
+
+    @Test
+    void shouldAddVictoryPointsToMovePoints_WhenGameStateIsVictory() {
+        // given
+        Move[][] scheme = {{A, Y}};
+        Long expectedScore = Y.points + VICTORY.points;
+
+        // when
+        Long playerTotalScore = GameSimulator.getPlayerTotalScore(scheme);
+
+        // then
+        assertThat(playerTotalScore).isEqualTo(expectedScore);
+    }
+
+    @Test
+    void shouldAddDrawPointsToMovePoints_WhenGameStateIsDefeat() {
+        // given
+        Move[][] scheme = {{A, X}};
+        Long expectedScore = X.points + DRAW.points;
+
+        // when
+        Long playerTotalScore = GameSimulator.getPlayerTotalScore(scheme);
+
+        // then
+        assertThat(playerTotalScore).isEqualTo(expectedScore);
+
+    }
+
+    @Test
+    void shouldAddNoPointsToMovePoints_WhenGameStateIsDefeat() {
+        // given
+        Move[][] scheme = {{B, X}};
+        Long expectedScore = X.points;
+
+        // when
+        Long playerTotalScore = GameSimulator.getPlayerTotalScore(scheme);
+
+        // then
+        assertThat(playerTotalScore).isEqualTo(expectedScore);
+
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException_WhenGivenSchemeHasInvalidFormat() {
+        // given
+        Move[][] scheme = {{A, B}, {A, B, C}};
+
+        // when / then
+        assertThatThrownBy(() -> GameSimulator.getPlayerTotalScore(scheme)).isInstanceOf(IllegalArgumentException.class);
+        // then
+    }
+
+}
---
+++ src/test/java/com/github/maypaw/advent_of_code/day2/SchemeProcessorTest.java
@@ -0,0 +1,51 @@
+package com.github.maypaw.advent_of_code.day2;
+
+import org.junit.jupiter.api.Test;
+
+import static com.github.maypaw.advent_of_code.day2.Move.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SchemeProcessorTest {
+
+    @Test
+    void shouldReturnDataInTwoDimensionalArrayScheme_WhenGettingProcessedData() {
+        // given
+        String scheme = """
+                        A X
+                        C Y
+                        B Z
+                        """;
+        Move[][] expectedScheme = {{A, X}, {C, Y}, {B, Z}};
+
+        // when
+        Move[][] processedData = SchemeProcessor.getProcessedData(scheme);
+
+        // then
+        assertThat(processedData).isEqualTo(expectedScheme);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException_WhenEmptySchemeGiven() {
+        // given
+        String scheme = """
+                        """;
+
+        // when / then
+        assertThatThrownBy(() -> SchemeProcessor.getProcessedData(scheme)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException_WhenSchemeWithContentDifferentThanMoveEnumNamesGiven() {
+        // given
+        String scheme = """
+                        A Content
+                        C Y
+                        B Z
+                        """;
+
+        // when / then
+        assertThatThrownBy(() -> SchemeProcessor.getProcessedData(scheme)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
