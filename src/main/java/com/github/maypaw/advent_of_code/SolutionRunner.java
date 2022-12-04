61dc3e6f7e1243f7f2b5879683b33951c5f1a0e3
Maja Pawlina <maja.anna.pawlina@gmail.com>
2022-12-04 01:01:59 +0100

    Updated SolutionRunner

--- src/main/java/com/github/maypaw/advent_of_code/SolutionRunner.java
+++ src/main/java/com/github/maypaw/advent_of_code/SolutionRunner.java
@@ -1,12 +1,17 @@
 package com.github.maypaw.advent_of_code;
 
 import com.github.maypaw.advent_of_code.day1.MaximumCaloriesFinder;
+import com.github.maypaw.advent_of_code.day2.GameSimulator;
+import com.github.maypaw.advent_of_code.day2.SchemeProcessor;
+import com.github.maypaw.advent_of_code.day2.WinningScheme;
 
 public class SolutionRunner {
 
     public static void main(String[] args) {
         System.out.println("Day 1 part 1 solution: " + MaximumCaloriesFinder.findMaxCaloriesSumPerElf());
         System.out.println("Day 1 part 2 solution: " + MaximumCaloriesFinder.findTopThreeCaloriesSumsPerElf());
+        System.out.println("Day 2 part 1 solution: " + GameSimulator.getPlayerTotalScore(
+                SchemeProcessor.getProcessedData(WinningScheme.CONTENT)));
     }
 
 }
