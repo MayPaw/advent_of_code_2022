429d90c5edbe48b9eb7c940f9425b423a805743b
Maja Pawlina <maja.anna.pawlina@gmail.com>
2022-12-04 01:01:23 +0100

    Implemented GameSimulator

--- src/main/java/com/github/maypaw/advent_of_code/day2/GameSimulator.java
+++ src/main/java/com/github/maypaw/advent_of_code/day2/GameSimulator.java
@@ -1,11 +1,55 @@
 package com.github.maypaw.advent_of_code.day2;
 
-import lombok.NoArgsConstructor;
+import java.util.Arrays;
+import java.util.Objects;
 
-@NoArgsConstructor
+import static com.github.maypaw.advent_of_code.day2.GameState.*;
+
 public class GameSimulator {
 
-    private final Long playerScore = 0L;
-    private final Long opponentScore = 0L;
+    private GameSimulator() {
+
+    }
+
+    public static Long getPlayerTotalScore(Move[][] winningScheme) {
+        validateScheme(winningScheme);
+        return Arrays.stream(winningScheme)
+                     .map(GameSimulator::countPlayerPoints)
+                     .reduce(0L, Long::sum);
+    }
+
+    private static Long countPlayerPoints(Move[] round) {
+        GameState gameState = getGameState(round);
+        if (gameState.equals(VICTORY)) {
+            return round[1].points + VICTORY.points;
+        }
+
+        if (gameState.equals(DEFEAT)) {
+            return round[1].points;
+        }
+
+        return round[1].points + DRAW.points;
+    }
+
+    private static void validateScheme(Move[][] winningScheme) {
+        Arrays.stream(winningScheme)
+              .forEach(round -> {
+                  if (Objects.isNull(round) || round.length != 2) {
+                      throw new IllegalArgumentException("Given winningScheme does not meet required format: [][]");
+                  }
+              });
+    }
+
+    private static GameState getGameState(Move[] round) {
+        if (round[0].moveName.equals(round[1].moveName)) {
+            return DRAW;
+        }
+
+        if (round[0].winsWith.equals(round[1].moveName)) {
+            return DEFEAT;
+        }
+
+        return VICTORY;
+    }
 
 }
